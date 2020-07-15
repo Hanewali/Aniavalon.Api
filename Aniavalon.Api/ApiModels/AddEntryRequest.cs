9c142f422253ad3b23b674fcea5e1b82d40d3fb0
Jakub Rumpel <jakub@rumpel.pl>
2020-07-15 23:13:07 +0200

    Added fields to addentryrequest model

--- Aniavalon.Api/ApiModels/AddEntryRequest.cs
+++ Aniavalon.Api/ApiModels/AddEntryRequest.cs
@@ -2,6 +2,8 @@ namespace Aniavalon.Api.ApiModels
 {
     public class AddEntryRequest
     {
-        
+        public int PersonId { get; set; }
+        public int SideId { get; set; }
+        public int PlayerCount { get; set; }
     }
 }
